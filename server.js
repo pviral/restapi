548180a910c782d55a22d0970d8058a6ae8e0890
evirapa <viral.patel@ericsson.com>
2016-06-25 12:25:55 -0700

    Adding remaining REST point
    1. Update and delete

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var path = require('path');
 var mongodb = require('mongodb');
 var bodyParser = require('body-parser');
 
-var objectID = mongodb.objectID;
+var objectId = mongodb.ObjectID;
 var CONTACTS_COLLECTION = "contacts";
 
 var app = express();
@@ -60,6 +60,39 @@ app.post("/contacts", function(req, res) {
       handleError(res, err.message, "Failed to create new contact.");
     } else {
       res.status(201).json(doc.ops[0]);
+    }
+  });
+});
+
+app.get("/contacts/:id", function(req, res) {
+  db.collection(CONTACTS_COLLECTION).findOne({ _id: new objectId(req.params.id) }, function(err, doc){
+    if (err) {
+      handleError(res, err.message, "Failed to get contact");
+    } else {
+      res.status(200).json(doc);
+    }
+  });
+});
+
+app.put("/contacts/:id", function(req, res) {
+  var updateDoc = req.body;
+  delete updateDoc._id;
+
+  db.collection(CONTACTS_COLLECTION).updateOne({_id: new objectId(req.params.id)}, updateDoc, function(err, doc){
+    if (err) {
+      handleError(res, err.message, "Failed to update contact");
+    } else {
+      res.status(204).end();
+    }
+  });
+});
+
+app.delete("/contacts/:id", function(req, res) {
+  db.collection(CONTACTS_COLLECTION).deleteOne({_id: new objectId(req.params.id)}, function(err, result){
+    if (err) {
+      handleError(res, err.message, "Failed to delete contact");
+    } else {
+      res.status(204).end();
     }
   });
 });
